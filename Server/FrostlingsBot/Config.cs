5c1537a2a5339b4359ea93a7133962ed8945d5ff
Stefan Janssen <StefanJanssen@outlook.com>
2018-04-02 16:08:35 +0200

    Create small bot to build on top off

---
+++ Server/FrostlingsBot/Config.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace FrostlingsBot
+{
+	public static class Config
+	{
+		private const string Path = "config.env.json";
+		private static bool _initialized;
+		private static JObject _data;
+		private static bool _throwErrors = false;
+
+		public static void Initialize(bool throwErrors=false)
+		{
+			_data = JObject.Parse(File.ReadAllText(Path));
+			_throwErrors = throwErrors;
+			_initialized = true;
+		}
+
+		public static T Get<T>(string path, T defaultValue)
+		{
+			if (!_initialized) Initialize();
+
+
+			JToken v = _data.SelectToken(path, _throwErrors);
+
+			return v == null ? defaultValue : (T) v.Value<T>();
+		}
+	}
+}
--- Server/FrostlingsBot/Program.cs
+++ Server/FrostlingsBot/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+using Newtonsoft.Json.Linq;
 
 namespace FrostlingsBot
 {
 	class Program
 	{
-		static void Main(string[] args)
+		private static DiscordClient discord;
+
+		private static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Config.Initialize(true);
+
+			MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+		}
+
+		private static async Task MainAsync(string[] args)
+		{
+			string token = Config.Get<string>("bot.client_secret", null);
+			Console.WriteLine(token);
+			discord = new DiscordClient(new DiscordConfiguration
+			{
+				Token     = token,
+				TokenType = TokenType.Bot
+			});
+			
+			discord.MessageCreated += async e =>
+			{
+				if (e.Message.Content.ToLower().StartsWith("ping"))
+					await e.Message.RespondAsync("pong!");
+			};
+			
+			await discord.ConnectAsync();
+			await Task.Delay(-1);
 		}
 	}
 }
